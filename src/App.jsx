98ffc38e13dde969038b36914cc95d26b5346421
caionikolas <Caio.kanu@hotmail.com>
2022-06-30 04:25:43 -0300

    index

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./components/header/Header"
+import Nav from "./components/nav/Nav"
+import About from "./components/about/About"
+import Experience from "./components/experience/Experience"
+import Portfolio from "./components/portfolio/Portfolio"
+import Testemonials from "./components/testemonials/Testemonials"
+import Contact from "./components/contact/Contact"
+import Footer from "./components/footer/Footer"
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Testemonials />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
