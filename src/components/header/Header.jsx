1042947d1f0cb2b722753d913728b2cbf5c67139
caionikolas <Caio.kanu@hotmail.com>
2022-07-01 19:49:26 -0300

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-import './header.css'
+import './header.css';
+import CTA from './CTA';
+import ME from '../../assets/me.png';
+import HeaderSocial from "./HeaderSocial";
 
 const Header = () => {
   return (
-    <Header>
+    <header>
       <div className="container header__container">
         <h5>Olá, Eu sou</h5>
         <h1>Caio Nikolas</h1>
         <h5 className="text-light">Front-End Developer</h5>
+        <CTA />
+        <HeaderSocial/>
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">Scroll Down</a>
       </div>
-    </Header>
+    </header>
   )
 }
 
