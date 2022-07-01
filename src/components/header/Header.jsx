4a7b240f9c70437cf14080ab0932ebd4bd6d9c86
caionikolas <Caio.kanu@hotmail.com>
2022-07-01 04:43:08 -0300

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,13 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Header>
+      <div className="container header__container">
+        <h5>Olá, Eu sou</h5>
+        <h1>Caio Nikolas</h1>
+        <h5 className="text-light">Front-End Developer</h5>
+      </div>
+    </Header>
   )
 }
 
