a0f3b58ab853a9bd2663dc2c11351c660cb37dfa
caionikolas <Caio.kanu@hotmail.com>
2022-07-09 15:37:58 -0300

    Services Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,19 @@
 /* ====== CHANGE BACKGROUND HEADER ====== */
 
 /* ====== SERVICES MODAL ====== */
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalClose = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick) {
+  modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener('click', () => {
+    modal(i)
+  })
+})
 /* ====== MIXITUP FILTER PORTFOLIO ====== */
 
 /* link active work */
